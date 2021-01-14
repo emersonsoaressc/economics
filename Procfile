7228eb76f1e24ff26d3d476ada2a776aad82de83
Emerson Soares <emerson.soares.sc@gmail.com>
2021-01-14 16:59:48 -0300

    commit11

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-import os
-with open(os.path.join('C:\Users\emers\OneDrive - UDESC Universidade do Estado de Santa Catarina\py_economics\','Procfile'), "w") as file1:
-    toFile = 'web: sh setup.sh && streamlit run home.py'
-    
-file1.write(toFile)
+web: sh setup.sh && streamlit run home.py
--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-mkdir -p ~ / .streamlit /
-echo "\ 
-[geral] \ n \ 
-email = \" emerson.soares.sc@gmail.com \ "\ n \ 
-"> ~ / .streamlit / credentials.toml
-echo "\ 
-[servidor] \ n \ 
-headless = true \ n \ 
-enableCORS = false \ n \ 
-port = $ PORT \ n \ 
-"> ~ / .streamlit / config.toml
+
+mkdir -p ~ /.streamlit/
+
+echo  " \
+[servidor] \ n \
+porta = $ PORT \ n \
+enableCORS = false \ n \
+headless = true \ n \
+\ n \
+"  >  ~ /.streamlit/config.toml
