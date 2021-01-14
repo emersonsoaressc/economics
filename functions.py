c546324898ff948507e1c311f9b7a2cf604f4ea5
Emerson Soares <emerson.soares.sc@gmail.com>
2021-01-14 18:32:12 -0300

    commit103

--- functions.py
+++ functions.py
@@ -5,15 +5,6 @@ import sgs
 import plotly.graph_objects as go
 import streamlit as st
 
-### ==== FUNÇÃO PARA GERAR BASE DE DADOS ==== ###
-def gera_base(ano_inicio, ano_fim):
-    inicio = ano_inicio
-    fim = ano_fim
-    cod_list = pd.read_csv('base_csv/cod_ipca.csv', sep=';').set_index('codigo')
-    df_sgs(cod_list['lista'], f'01/01/{inicio}', f'01/01/{fim}', 'ipca')
-    df_base = pd.read_csv('base_csv/base_ipca.csv', encoding='UTF-8', sep=',', index_col=0)
-    return df_base
-
 
 ### ==== FUNÇÃO PARA CRIAR DATAFRAMES A PARTIR DE BASE DE DADOS SGS BACEN ==== ###
 def df_sgs(lista_sgs,ano_inicio, ano_fim, name):
@@ -27,7 +18,17 @@ def df_sgs(lista_sgs,ano_inicio, ano_fim, name):
         df1 = df1.rename(columns={f'{split[0]}':f'{split[1]}'})
         df = pd.concat([df,df1], axis=1)
     return df.to_csv(f'base_csv/base_{name}.csv')
-    
+
+
+### ==== FUNÇÃO PARA GERAR BASE DE DADOS ==== ###
+def gera_base(ano_inicio, ano_fim):
+    inicio = ano_inicio
+    fim = ano_fim
+    cod_list = pd.read_csv('base_csv/cod_ipca.csv', sep=';').set_index('codigo')
+    df_sgs(cod_list['lista'], f'01/01/{inicio}', f'01/01/{fim}', 'ipca')
+    df_base = pd.read_csv('base_csv/base_ipca.csv', encoding='UTF-8', sep=',', index_col=0)
+    return df_base
+
 
 ### ==== FUNÇÃO PARA CRIAR GRÁFICOS DO PLOTLY A PARTIR DE BASE DE DADOS SGS BACEN ==== ###
 def graf_plotly(data_frame, titulo):
