d2f54e4c93f02733efc49972ab065eba3cb23adf
Emerson Soares <emerson.soares.sc@gmail.com>
2021-01-15 18:31:00 -0300

    v5

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ import sgs
 import plotly.graph_objects as go
 import options as opt
 import locale
-locale.setlocale(locale.LC_ALL, 'pt_BR')
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 
 ##### ========== FUNÇÕES BÁSICAS ========== #####
