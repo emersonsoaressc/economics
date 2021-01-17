0d1bf284f119f011afd8f6d557d70c02f4ea3c96
Emerson Soares <emerson.soares.sc@gmail.com>
2021-01-17 20:45:14 -0300

    v1

--- covidometro.py
+++ covidometro.py
@@ -28,6 +28,17 @@ df = df.iloc[0:10]
 
 def pag_covid():
 # Fazendo a verificação do subtópico abordado
+    covid = Covid()
+    covid_paises = covid.list_countries()
+    covid_paises = pd.DataFrame(covid_paises)
+    covid_paises = covid_paises.set_index('id')
+    df = pd.DataFrame()
+    for i in covid_paises['name']:
+        df1 = covid.get_status_by_country_name(i)
+        df1 = pd.DataFrame.from_dict(df1, orient='index').T
+        df = pd.concat([df, df1])
+    df['% Mortalidade'] = df['deaths'] / df['confirmed'] * 100
+    df = df.iloc[0:10]
     lst_paises = st.sidebar.selectbox('Selecione o tópico que deseja abordar:',covid_paises)
     period = st.sidebar.slider('select',1980,2021,(2019,2021))
     st.write(df)
