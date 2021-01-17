9aa492f4c07487f677eb670518d438ccc76b68c9
Emerson Soares <emerson.soares.sc@gmail.com>
2021-01-17 20:48:33 -0300

    v1

--- covidometro.py
+++ covidometro.py
@@ -9,25 +9,7 @@ from covid import Covid
 ### ========= TRATAMENTO DE DADOS REFERENTE AO COVID ========= ###
 
 # Listando países para criar dataframe
-covid = Covid()
-covid_paises = covid.list_countries()
-covid_paises = pd.DataFrame(covid_paises)
-covid_paises = covid_paises.set_index('id')
-df = pd.DataFrame()
-for i in covid_paises['name']:
-  df1 = covid.get_status_by_country_name(i)
-  df1 = pd.DataFrame.from_dict(df1, orient='index').T
-  df = pd.concat([df, df1])
-df['% Mortalidade'] = df['deaths'] / df['confirmed'] * 100
-df = df.iloc[0:10]
-
-### ========= ARQUITETURA DA PÁGINA ========= ###
-
-
-### ==== FUNÇÃO PARA CRIAR PÁGINA COVID ==== ###
-
-def pag_covid():
-# Fazendo a verificação do subtópico abordado
+def cria_df_covid():
     covid = Covid()
     covid_paises = covid.list_countries()
     covid_paises = pd.DataFrame(covid_paises)
@@ -36,9 +18,19 @@ def pag_covid():
     for i in covid_paises['name']:
         df1 = covid.get_status_by_country_name(i)
         df1 = pd.DataFrame.from_dict(df1, orient='index').T
-        df = pd.concat([df, df1])
+    df = pd.concat([df, df1])
     df['% Mortalidade'] = df['deaths'] / df['confirmed'] * 100
     df = df.iloc[0:10]
+    df
+
+### ========= ARQUITETURA DA PÁGINA ========= ###
+
+
+### ==== FUNÇÃO PARA CRIAR PÁGINA COVID ==== ###
+
+def pag_covid():
+# Fazendo a verificação do subtópico abordado
+    df = cria_df_covid()
     lst_paises = st.sidebar.selectbox('Selecione o tópico que deseja abordar:',covid_paises)
     period = st.sidebar.slider('select',1980,2021,(2019,2021))
     st.write(df)
