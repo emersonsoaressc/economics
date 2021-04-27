c78e13b2c39e3b452c544e9c568ca0a8862b3f11
Emerson Soares <emerson.soares.sc@gmail.com>
2021-04-27 17:29:13 -0300

    v1

--- carteira.py
+++ carteira.py
@@ -12,10 +12,10 @@ import plotly.graph_objects as go
 
 def pag_carteira():
 # ESCOLHENDO ATIVOS PARA ENTRAR NA CARTEIRA
-    stocks = pd.read_csv('base_csv/stocks.csv')
+    stocks_csv = pd.read_csv('base_csv/stocks.csv')
     n_stocks = st.sidebar.slider('Quantos ativos irá compor a carteira?:', 1,10,(1))
-    st.sidebar.multiselect('Insira o ticker das ações na carteira:',stocks)
+    stocks = st.sidebar.multiselect('Insira o ticker das ações na carteira:',stocks_csv)
     period = st.sidebar.slider('A partir de qual ano deseja analisar?:',2000,2022,(2015))
-
+    st.write('Você escolheu:', stocks)
 
     
