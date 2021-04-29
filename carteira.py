573fe376e160756fc1592969d7528d64d642a34f
Emerson Soares <emerson.soares.sc@gmail.com>
2021-04-29 12:10:23 -0300

    v1

--- carteira.py
+++ carteira.py
@@ -5,8 +5,6 @@ from pandas_datareader import data
 from func_pyeconomics import  graf_plotly, gera_carteira, normaliza_carteira
 import plotly.graph_objects as go
 
-### ========= ARQUITETURA DA PÁGINA ========= ###
-
 
 ### ==== FUNÇÃO PARA CRIAR PÁGINA DE ANÁLISE DE CARTEIRA ==== ###
 
@@ -19,16 +17,20 @@ def pag_carteira():
     dataframe = gera_carteira(stocks,period)
     df_norm = normaliza_carteira(dataframe)
     benchmark = pd.DataFrame(df_norm['^BVSP'])
+    benchmark.rename(columns={'IBOVESPA'}, inplace=True)
     carteira = df_norm.drop(columns='^BVSP')
     carteira['CARTEIRA'] = carteira.sum(axis=1) / len(carteira.columns)
     cart = pd.DataFrame(carteira['CARTEIRA'])
     cart_bench = pd.concat([cart, benchmark], axis = 1)
-    parag1 = st.write(
-        'Nesta página você poderá realizar a simulação de uma carteira de ações e compará-la com o benchmark (Ibovespa). Basta escolher os ativos e o período inicial na barra lateral. Os dados são extraídos de diversas fontes, sendo a fonte principal o YAHOO FINANCE. Também será realizada diversas análises referente ao portfólio'
+    ### ========= ARQUITETURA DA PÁGINA ========= ### 
+    st.write(
+        'Nesta página você poderá realizar a simulação de uma carteira de ações e compará-la com o benchmark (Ibovespa). Basta escolher os ativos e o período inicial na barra lateral. Os dados são extraídos de diversas fontes, sendo a fonte principal o YAHOO FINANCE. Também será realizada diversas análises referente ao portfólio.'
     )
-    parag2 = st.write(
+    ### ========= COMPARATIVO CARTEIRA X IBOVESPA ========= ### 
+    st.write(
         'Segue abaixo o gráfico "COMPARATIVO - CARTEIRA X IBOVESPA"'
     )
     st.write(graf_plotly(cart_bench))
+
     st.write(cart_bench)
     st.write(carteira)
