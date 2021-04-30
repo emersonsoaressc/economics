b0412d047aa7dfbb5123f96ce6f22ee335414a9f
Emerson Soares <emerson.soares.sc@gmail.com>
2021-04-30 12:12:18 -0300

    v1

--- carteira.py
+++ carteira.py
@@ -14,16 +14,7 @@ def pag_carteira():
     stocks_csv = pd.read_csv('base_csv/stocks.csv')
     stocks = st.sidebar.multiselect('Insira o ticker das ações na carteira:',stocks_csv)
     period = st.sidebar.slider('A partir de qual ano deseja analisar?:',2010,2021,(2010))
-    n_stocks = len(stocks)
-    dataframe = gera_carteira(stocks,period)
-    df_norm = normaliza_carteira(dataframe)
-    benchmark = pd.DataFrame(df_norm['^BVSP'])
-    benchmark.rename(columns={'^BVSP':'IBOVESPA'}, inplace=True)
-    carteira = df_norm.drop(columns='^BVSP')
-    carteira['CARTEIRA'] = carteira.sum(axis=1) / len(carteira.columns)
-    cart = pd.DataFrame(carteira['CARTEIRA'])
-    cart_bench = pd.concat([cart, benchmark], axis = 1)
-    ativos_ibov = pd.concat([carteira, benchmark], axis = 1)
+    n_stocks = len(stocks)  
     st.write(
         'Nesta página você poderá realizar a simulação de uma carteira de ações e compará-la com o benchmark (Ibovespa). Basta escolher os ativos e o período inicial na barra lateral. Os dados são extraídos de diversas fontes, sendo a fonte principal o YAHOO FINANCE. Também será realizada diversas análises referente ao portfólio.'
     )
@@ -46,12 +37,21 @@ def pag_carteira():
             st.write(
                 '"COMPARATIVO - CARTEIRA X IBOVESPA"'
             )
+            dataframe = gera_carteira(stocks,period)
+            df_norm = normaliza_carteira(dataframe)
+            benchmark = pd.DataFrame(df_norm['^BVSP'])
+            benchmark.rename(columns={'^BVSP':'IBOVESPA'}, inplace=True)
+            carteira = df_norm.drop(columns='^BVSP')
+            carteira['CARTEIRA'] = carteira.sum(axis=1) / len(carteira.columns)
+            cart = pd.DataFrame(carteira['CARTEIRA'])
+            cart_bench = pd.concat([cart, benchmark], axis = 1)
+            ativos_ibov = pd.concat([carteira, benchmark], axis = 1)
             st.write(graf_plotly(cart_bench))
             st.write(
                 '"COMPARATIVO - ATIVOS X IBOVESPA"'
             )
             st.write(graf_plotly(ativos_ibov))
-            st.write(ativos_ibov)
+            st.write(carteira)
             ### ========= TAXA DE RETORNO DA CARTEIRA ========= ###
             st.write(
             '"TAXA DE RETORNO DA CARTEIRA"'
