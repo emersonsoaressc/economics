a3a157dd14c326191c43109de62f2093898a9b57
Emerson Soares <emerson.soares.sc@gmail.com>
2021-04-30 18:57:25 -0300

    v1

--- carteira.py
+++ carteira.py
@@ -46,10 +46,7 @@ def pag_carteira():
             cart_bench = pd.concat([cart, benchmark], axis = 1)
             ativos_ibov = pd.concat([carteira, benchmark], axis = 1)
             st.write(graf_plotly(cart_bench))
-            st.markdown(
-                '***COMPARATIVO - ATIVOS X IBOVESPA}***'
-
-            )
+            st.markdown('***COMPARATIVO - ATIVOS X IBOVESPA***',textcolor{'#228B22'})
             st.write(graf_plotly(df_norm))
             ### ========= TAXA DE RETORNO DA CARTEIRA ========= ###
             st.write(
