7f52996ef45b6573c0d7f56b1b5d32b6889dfe7c
Emerson Soares <emerson.soares.sc@gmail.com>
2021-04-30 19:01:05 -0300

    v1

--- carteira.py
+++ carteira.py
@@ -49,14 +49,10 @@ def pag_carteira():
             st.markdown('***COMPARATIVO - ATIVOS X IBOVESPA***')
             st.write(graf_plotly(df_norm))
             ### ========= TAXA DE RETORNO DA CARTEIRA ========= ###
-            st.write(
-            '"TAXA DE RETORNO DA CARTEIRA"'
-            )
+            st.markdown('***TAXA DE RETORNO DA CARTEIRA***')
             tx_retorno = (df_norm / df_norm.shift(1)) - 1
             retorno_anual = tx_retorno.mean() * 246
             st.write(retorno_anual)
             st.write(tx_retorno)
                 ### ========= CORRELAÇÃO ENTRE OS ATIVOS ========= ###
-            st.write(
-            '"CORRELAÇÃO ENTRE OS ATIVOS"'
-            )
+            st.markdown('***CORRELAÇÃO ENTRE OS ATIVOS***')
