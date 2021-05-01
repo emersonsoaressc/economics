ec0a8fe9d854b917568a776e7b0e8560cec51164
Emerson Soares <emerson.soares.sc@gmail.com>
2021-05-01 11:47:10 -0300

    v1

--- func_pyeconomics.py
+++ func_pyeconomics.py
@@ -83,6 +83,6 @@ def normaliza_carteira(dataframe):
 
 
 def graf_corr(dataframe):
-    fig = px.imshow(dataframe,colorscale='RdBu')
+    fig = px.imshow(dataframe,color_continuous_scale='RdBu')
     fig.show()
     return fig
