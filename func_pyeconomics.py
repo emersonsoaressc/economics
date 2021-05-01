b4f63956be863c0851953f31a8ab4aa0e9eddb30
Emerson Soares <emerson.soares.sc@gmail.com>
2021-05-01 14:31:02 -0300

    v1

--- func_pyeconomics.py
+++ func_pyeconomics.py
@@ -87,11 +87,11 @@ def graf_corr(dataframe):
     im = ax.imshow(dataframe.values)
     ax.set_xticks(range(len(dataframe)))
     ax.set_yticks(range(len(dataframe)))
-    ax.set_xticklabels(dataframe.columns,rotation='vertical')
-    ax.set_yticklabels(dataframe.columns)
+    ax.set_xticklabels(dataframe.columns,rotation='vertical', size=5)
+    ax.set_yticklabels(dataframe.columns, size=5)
     for i in range(len(dataframe)):
         for j in range(len(dataframe)):
             text = ax.text(j, i, round(dataframe.values[i, j],3),
-                        ha="center", va="center", color="w", size=10)
+                        ha="center", va="center", color="w", size=5)
     fig.tight_layout()
     return fig
